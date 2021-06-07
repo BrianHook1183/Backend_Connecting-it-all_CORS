9e83fd2a7ac86d04e4d2c500c68693466085b17a
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-07 16:15:32 -0500

    submitted soltuion | added notes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// const cors = require("cors");
 const app = express();
 
 const plantsRouter = require("./plants/plants.router");
@@ -6,6 +7,8 @@ const usersRouter = require("./users/users.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+// This would enable CORS for entire app instead of by method/route/router
+// app.use(cors());
 app.use(express.json());
 
 app.use("/plants", plantsRouter);
--- src/plants/plants.router.js
+++ src/plants/plants.router.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const controller = require("./plants.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
 //TODO make the necessary changes to enable CORS for the entire router.
 
+// enables CORS for all routes in this router
+router.use(cors());
+
 router
   .route("/:plantId")
+  //enables CORS for all methods on this route
+  // .all(cors())
   .get(controller.read)
   .put(controller.update)
   .delete(controller.delete)
@@ -13,6 +19,8 @@ router
 
 router
   .route("/")
+  //enables CORS for all methods on this route
+  // .all(cors())
   .get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
